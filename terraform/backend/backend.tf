ffcfc4cd13e5c0c86a81351b10652f8c581460ef
callme-mud4551r <ubuntu@ip-10-0-132-52.ap-south-1.compute.internal>
2026-06-04 05:55:40 +0000

    added terraform/backend.tf

--- terraform/backend/backend.tf
+++ terraform/backend/backend.tf
@@ -1,7 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "your-terraform-state-bucket"
-    key    = "eks/terraform.tfstate"
-    region = "ap-south-1"
+    bucket         = "terraform-state-bucket"
+    key            = "eks/terraform.tfstate"
+    region         = "ap-south-1"
+    dynamodb_table = "terraform-locks"
   }
 }
