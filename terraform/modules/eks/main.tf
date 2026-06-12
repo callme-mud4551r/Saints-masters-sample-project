610b256dc92a611c0c875305e253177ec80384dd
naveensynergy-grp <Naveen@synergyresourcesgrp.com>
2026-06-12 15:10:18 +0530

    added iam role

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -79,34 +79,8 @@ resource "aws_eks_node_group" "main" {
     aws_iam_role_policy_attachment.worker_node,
     aws_iam_role_policy_attachment.cni,
     aws_iam_role_policy_attachment.ecr,
-    aws_iam_role_policy_attachment.node_s3,
   ]
 }
 
 
-#Create the S3 Inline/Custom Policy for your bucket
-resource "aws_iam_policy" "eks_s3_upload_policy" {
-  name        = "${var.Three-tier-app}-s3-upload-policy"
-  description = "Allows EKS worker nodes to upload images to the S3 bucket"
 
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Action = [
-          "s3:PutObject",
-          "s3:PutObjectAcl"
-        ]
-        # Replace "your-actual-s3-bucket-name" with your variable or bucket resource name
-        Resource = "arn:aws:s3:::eks-app-uploads-2026/uploads/*"
-      }
-    ]
-  })
-}
-
-# 2. Attach the S3 Policy to your existing worker node role
-resource "aws_iam_role_policy_attachment" "node_s3" {
-  policy_arn = aws_iam_policy.eks_s3_upload_policy.arn
-  role       = aws_iam_role.nodes.name
-}
